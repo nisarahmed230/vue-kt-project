de328c88ccdddf2faa801193238566d8f9186ffd
Ahmed <Nisar.Ahmed@lumen.com>
2025-05-29 13:42:40 +0530

    Stable - Done with Logo

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,9 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-4">
-    <a class="navbar-brand" href="#">Employee Portal</a>
+    <a class="navbar-brand d-flex align-items-center" href="#">
+      <img src="../assets/logo.png" alt="WorkSphere Logo" width="50" height="50" class="me-2" />
+      WorkSphere
+    </a>
     <div class="collapse navbar-collapse">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
--- src/pages/EmployeeTable.vue
+++ src/pages/EmployeeTable.vue
@@ -9,29 +9,19 @@
           <th>Phone</th>
           <th>Department</th>
           <th>Designation</th>
-          <th>DOB</th>
-          <th>Marital Status</th>
-          <th>Street</th>
           <th>City</th>
-          <th>State</th>
-          <th>Zip</th>
           <th>Actions</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="emp in employees" :key="emp.employeeId">
           <td>{{ emp.employeeId }}</td>
-          <td>{{ emp.firstName }} {{ emp.lastName }}</td>
+          <td>{{ capitalize(emp.firstName) }} {{ capitalize(emp.lastName) }}</td>
           <td>{{ emp.email }}</td>
           <td>{{ emp.phoneNumber }}</td>
           <td>{{ emp.department }}</td>
           <td>{{ emp.designation }}</td>
-          <td>{{ emp.employeeDetails?.dateOfBirth }}</td>
-          <td>{{ emp.employeeDetails?.maritalStatus }}</td>
-          <td>{{ emp.employeeDetails?.address?.street }}</td>
           <td>{{ emp.employeeDetails?.address?.city }}</td>
-          <td>{{ emp.employeeDetails?.address?.state }}</td>
-          <td>{{ emp.employeeDetails?.address?.zipCode }}</td>
           <td>
             <button @click="$emit('deleteEmployee', emp.employeeId)" class="btn btn-danger btn-sm">
               Delete
@@ -47,6 +37,7 @@
 </template>
 
 <script setup lang="ts">
+import { capitalize } from 'vue';
 import type { EmployeeDto } from '../types/EmployeeDto'
 
 defineProps<{
